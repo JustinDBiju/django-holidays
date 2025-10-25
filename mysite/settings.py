8f70a4263fbac1e3cbbc93c071d351c4e26922fb
Justin D Biju <justindbiju0772@gmail.com>
2025-10-25 12:10:10 +0530

    Added email pass and host mail directly

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,7 +134,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = "justindbijutest@gmail.com"
+EMAIL_HOST_PASSWORD = "uvpa fydy yvhh tdmo"
 
 
